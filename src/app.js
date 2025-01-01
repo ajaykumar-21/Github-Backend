6c73a29fb17c936bafeab685d1ee211c941ae04a
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-01-01 21:12:14 +0530

    create basic url to test app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const app = express();
+
+// const PORT = 8001;
+
+// app.use("/", (req, res) => {
+//   res.send("welcome to home");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// module.exports = app;
