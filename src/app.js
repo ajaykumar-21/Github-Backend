fb28decd87fd076a84f50529e66d7190a8b5f0d9
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-01-02 16:24:04 +0530

    mongoDB connection implement is ready

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/github_user")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB connection error:", error));
 
 app.use("/", (req, res) => {
   res.send("welcome to home");
